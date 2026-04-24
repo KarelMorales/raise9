c25fe974dcb59b3cdecf809583ae6caf26eb4f14
c_keiprx <karelmorales88@gmail.com>
2026-04-24 22:15:47 +0800

    cms setup

---
+++ raisewesmin/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(7h8lh8v%#qk(hflln^9kto14%zsj7_+(9x1p@f%k0*u094c6#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
